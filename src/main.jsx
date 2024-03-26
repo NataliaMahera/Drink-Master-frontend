b45e4138e919374f9be65b304d888661b9781e7c
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2024-03-26 10:00:17 +0100

    add new name

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import './index.css';
 import './i18n.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/frontend-drink-master">
+  <BrowserRouter basename="/Drink-Master-frontend">
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <App />
